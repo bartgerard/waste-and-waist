b21c6433a05d70313dcf817898718e4626599221
bartgerard <bart.gerard@gmail.com>
2023-11-17 22:26:55 +0100

    :bug: Make swagger usable again

--- app/src/main/java/be/ww/app/TestRestController.java
+++ app/src/main/java/be/ww/app/TestRestController.java
@@ -1,4 +1,4 @@
-package be.ww.household.command.web;
+package be.ww.app;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
