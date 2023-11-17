4d3da32bbfd149438e8fe490619cdb2546704f1f
b0wdyy <vandaelbodhi@hotmail.com>
2023-11-17 22:22:40 +0100

    :sparkles: Feat: new angular project, prettier, eslint, primeng

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core'
+import { PrimeNGConfig } from 'primeng/api'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'client'
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true
+    this.primengConfig.zIndex = {
+      modal: 1100,
+      overlay: 1000,
+      menu: 1000,
+      tooltip: 1100,
+    }
+  }
+}
