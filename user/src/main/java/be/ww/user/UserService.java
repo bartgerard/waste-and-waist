64154137fcb739025c980935308bb91a2f6b291c
SanderVanderlinden <sander-vanderlinden@hotmail.com>
2023-11-18 10:27:05 +0100

    Format white lines

--- user/src/main/java/be/ww/user/UserService.java
+++ user/src/main/java/be/ww/user/UserService.java
@@ -1,31 +1,32 @@
 package be.ww.user;
 
-import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-	UserRepository repository;
+    UserRepository repository;
 
 
-	public User registerUser(String username, String password){
-		User user = new User(username, password);
-		return repository.save(user);
-	}
-	public User loginUser(String username,String password){
-		User foundUser = repository.findUserByUsername(username)
-				.orElseThrow(()->  new IllegalArgumentException("user does not exist"));
-		if(foundUser.password.equals(password)) {
-			return foundUser;
-		}else{
-			throw  new IllegalArgumentException("wrong password");
-		}
-	}
-	public User getUser(String userId){
-		return repository.findUserByUserId(userId)
-				.orElseThrow(()-> new IllegalArgumentException("User does not exist"));
-	}
+    public User registerUser(String username, String password) {
+        User user = new User(username, password);
+        return repository.save(user);
+    }
+
+    public User loginUser(String username, String password) {
+        User foundUser = repository.findUserByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("user does not exist"));
+        if (foundUser.password.equals(password)) {
+            return foundUser;
+        } else {
+            throw new IllegalArgumentException("wrong password");
+        }
+    }
+
+    public User getUser(String userId) {
+        return repository.findUserByUserId(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User does not exist"));
+    }
 }
