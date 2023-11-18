db3d1b7a2fb31e717fd7814e7e36dd630847e069
SanderVanderlinden <sander-vanderlinden@hotmail.com>
2023-11-18 17:59:10 +0100

    Add fields to AddmealCommand

--- planner/api/src/main/java/be/ww/planner/api/command/AddMealCommand.java
+++ planner/api/src/main/java/be/ww/planner/api/command/AddMealCommand.java
@@ -2,12 +2,15 @@ package be.ww.planner.api.command;
 
 
 import be.ww.shared.type.MealId;
+import be.ww.shared.type.RecipeId;
 import be.ww.shared.type.ScheduleId;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 public record AddMealCommand(
         @TargetAggregateIdentifier
         ScheduleId scheduleId,
-        MealId mealId
+        MealId mealId,
+        RecipeId recipeId,
+        int amountOfPeople
 ) {
 }
