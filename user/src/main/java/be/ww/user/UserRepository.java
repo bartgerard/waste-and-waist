7528c50b17eed23c8456096c26c0c85be6ab892d
SanderVanderlinden <sander-vanderlinden@hotmail.com>
2023-11-18 10:25:40 +0100

    Fix typo: removed space

--- user/src/main/java/be/ww/user/UserRepository.java
+++ user/src/main/java/be/ww/user/UserRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository  extends JpaRepository<User,String> {
+public interface UserRepository extends JpaRepository<User,String> {
 	Optional<User> findUserByUsername(String username);
 	Optional<User> findUserByUserId(String userId);
 
