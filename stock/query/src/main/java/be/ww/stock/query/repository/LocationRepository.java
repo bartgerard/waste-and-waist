de29586841f0c0486ab32e536c121c6a4df62d77
RikGeboers <{ID}+{username}@users.noreply.github.com>
2023-11-18 18:40:44 +0100

    productAdded Event listener stock

--- stock/query/src/main/java/be/ww/stock/query/repository/LocationRepository.java
+++ stock/query/src/main/java/be/ww/stock/query/repository/LocationRepository.java
@@ -7,4 +7,8 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 public interface LocationRepository extends ElasticsearchRepository<LocationDocument, String> {
 
 	Optional<LocationDocument> findByLocationId(String locationId);
+
+	Optional<LocationDocument> findByProductId(String productId);
+
+	Optional<LocationDocument> findByProvisionId(String provisionId);
 }
