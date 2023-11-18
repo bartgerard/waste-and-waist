d506189d80b298e13e14e203e75acde1cdb75f86
velizartodorov <willking66@gmail.com>
2023-11-18 17:11:17 +0100

    :art: add location stubs

--- client/src/modules/household/components/overview/household.component.ts
+++ client/src/modules/household/components/overview/household.component.ts
@@ -3,6 +3,11 @@
 import { Component, OnInit } from '@angular/core'
 import { PrimeNGConfig } from 'primeng/api'
 
+interface Location {
+  locationId: number;
+  name: string;
+}
+
 interface Appliance {
   type: string;
 }
@@ -16,9 +21,18 @@ export class HouseholdComponent implements OnInit {
 
   title = 'client'
   appliances: Appliance[];
+  locations: Location[];
   selectedAppliance!: Appliance;
+selectedLocation!: Location;
 
   constructor(private primengConfig: PrimeNGConfig) {
+    this.locations = [
+      { locationId: 1, name: 'Villa in Turkey' },
+      { locationId: 2, name: 'House in Wallonia' },
+      { locationId: 3, name: 'Apartment in New York' },
+      { locationId: 4, name: 'Cottage in Brazil' },
+      { locationId: 5, name: ' Mansion in Abu Dhabi' },
+    ]
     this.appliances = [
       { type: 'Microwave' },
       { type: 'Coffee machine' },
