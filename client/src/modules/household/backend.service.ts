6f3d9f97ad63a8dbdcfcc4dadee9276097d9a9f7
velizartodorov <willking66@gmail.com>
2023-11-19 10:14:24 +0100

    :art: add functions to service

--- client/src/modules/household/backend.service.ts
+++ client/src/modules/household/backend.service.ts
@@ -11,10 +11,34 @@ export class BackendService {
   constructor(private http: HttpClient) {}
 
   getData(): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/endpoint`)
+    return this.http.get<any>(`${this.baseUrl}/locations`)
   }
 
   sendData(data: any): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/endpoint`, data)
+  }
+
+  createUser(userData: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/users/register`, userData)
+  }
+
+  userLogin(loginData: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/users/login`, loginData)
+  }
+
+  getHouseholdsByUserId(userId: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/house-holds/by-user/${userId}`)
+  }
+
+  createHousehold(householdData: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/house-holds/`, householdData)
+  }
+
+  createHouseholdLocations(locationData: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/locations`, locationData)
+  }
+
+  getHouseholdLocations(houseHoldId: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/house-holds/${houseHoldId}/locations`)
   }
 }
